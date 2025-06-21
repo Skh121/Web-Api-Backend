f9fd941f1e76c09b011cecd728108a282c61e038
Skh121 <107852103+Skh121@users.noreply.github.com>
2025-06-21 21:05:39 +0545

    Add admin routes for user and trade management with authentication middleware

---
+++ routes/admin/adminRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const { getAllUsers, getOneUser, updateUser, deleteUser } = require("../../controllers/admin/userManagement");
+const { authorizedUser, isAdmin } = require("../../middlewares/authenticateUser");
+const router = express.Router()
+
+router.get("/",authorizedUser,isAdmin,getAllUsers);
+router.get("/:id",getOneUser);
+router.put("/:id",updateUser);
+router.delete("/:id",deleteUser);
+
+module.exports = router
